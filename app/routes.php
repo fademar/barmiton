d994d4f32c71991633230ac231440c9e86822663
Pezeron Thibault <thib.pezeron@gmail.com>
2016-08-08 17:47:00 +0200

    Co facebook ok et formulaire en cours

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,6 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+		['GET|POST', '/users/inscription/', 'Users#usersinscription', 'users_usersinscription'],
+		['GET|POST', '/users/connexion/', 'Users#usersconnexion', 'users_usersconnexion'],
 	);
