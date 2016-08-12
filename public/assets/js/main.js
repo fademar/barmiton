14b3a33cf07826950492836393696e0a84ed6f6b
Fabrice DEMARTHON <fdemarthon@gmail.com>
2016-08-13 01:06:12 +0200

    ?

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
 		    prefetch: 'http://localhost/barmiton/public/data/fruits/'
 		},
 		{
-		    name: 'épices',
+		    name: 'epices',
 		    prefetch: 'http://localhost/barmiton/public/data/epices/'
 		},
 	]);
