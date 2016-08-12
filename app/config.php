f9ce2f16dc7f2ac9eb63856ce69e0f8dada60af6
Fabrice Demarthon <fdemarthon@gmail.com>
2016-08-12 07:19:46 +0200

    motif config pour mac

--- app/config.php
+++ app/config.php
@@ -4,7 +4,7 @@ $w_config = [
    	//information de connexion à la bdd
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
     'db_user' => 'root',							//nom d'utilisateur pour la bdd
-    'db_pass' => '',								//mot de passe de la bdd
+    'db_pass' => 'root',								//mot de passe de la bdd
     'db_name' => 'barmiton',								//nom de la bdd
     'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
@@ -19,7 +19,7 @@ $w_config = [
 	'security_login_route_name' => 'login',			//nom de la route affichant le formulaire de connexion
 
 	// configuration globale
-	'site_name'	=> '', 								// contiendra le nom du site
+	'site_name'	=> 'barmiton', 								// contiendra le nom du site
 ];
 // Coucou les enculés
 require('routes.php');
